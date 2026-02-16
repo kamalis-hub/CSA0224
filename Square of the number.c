eae6f1a4feb168e8836929d063a0c0761776f9d3
kamalis-hub <kamalisk2017@gmail.com>
2026-02-16 22:20:26 +0530

    Create Square of the number.c

---
+++ Square of the number.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+ {
+    int n, square;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    square = n* n;
+    printf("Square = %d\n", square);
+    return 0;
+ }
