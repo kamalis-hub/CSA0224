3fa364b627dc54e66c7de3008e1d58ea406820aa
kamalis-hub <kamalisk2017@gmail.com>
2026-02-17 07:58:33 +0530

    Subtraction.c

---
+++ Subtraction.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int n, i,k;
+
+    printf("Enter a number: ");
+    scanf("%d%d", &n,&i);
+k=n-i;
+   {
+        printf("The subtraction of two numbers is:\n", k);
+    }
+
+    return 0;
+}
