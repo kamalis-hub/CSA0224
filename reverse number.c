3653555aa65532946dd493ecc517f7d31887664b
kamalis-hub <kamalisk2017@gmail.com>
2026-02-18 12:55:40 +0530

    Create reverse number.c

---
+++ reverse number.c
@@ -0,0 +1,15 @@
+#include<stdio.h> 
+int main(){ 
+int num,remainder,rev=0; 
+printf("enter the number:"); 
+scanf("%d",&num); 
+while(num!=0) 
+{ 
+} 
+remainder=num%10; 
+rev=rev*10+remainder; 
+num=num/10; 
+printf("the reversed number is:%d\n",rev); 
+return 0; 
+} 
+PALINDROME NUMB
